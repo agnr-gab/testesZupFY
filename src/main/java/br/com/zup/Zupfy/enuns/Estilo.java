ce2e260308f9fb9abe232f30a4dd1a18448e380d
Arê Nascimento <aretuza.gab@gmail.com>
2021-12-10 14:44:38 -0300

    Corrigir Enuns

--- src/main/java/br/com/zup/Zupfy/enuns/Estilo.java
+++ src/main/java/br/com/zup/Zupfy/enuns/Estilo.java
@@ -1,5 +1,11 @@
 package br.com.zup.Zupfy.enuns;
 
 public enum Estilo {
-    ROCK, POP, KPOP, FUNCK, JAZZ, BLUES, HipHop;
+    ROCK,
+    POP,
+    KPOP,
+    FUNK,
+    JAZZ,
+    BLUES,
+    HIPHOP;
 }
